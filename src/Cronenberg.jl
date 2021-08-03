54277ad11cae5159759390eba92e93f817b8b6be
Daniel C. Jones <dcjones@cs.washington.edu>
2021-08-03 10:58:58 -0700

    Many fixes to iteration.

--- src/Cronenberg.jl
+++ src/Cronenberg.jl
@@ -1,6 +1,7 @@
 module Cronenberg
 
 using StaticArrays
+using Random: shuffle!
 
 const Tile = Tuple{UnitRange{Int}, UnitRange{Int}}
 
@@ -38,54 +39,6 @@ end
 
 
 """
-Return the cell state at (i,j), and 0 if it's out of bounds.
-"""
-function getstate(world_state::Matrix{Int32}, i::Int, j::Int)
-    m, n = size(world_state)
-    return ((1 <= i <= m) && (1 <= j <= n)) ? world_state[i,j] : Int32(0)
-end
-
-
-"""
-Check each neighbor of (i,j) and record whether the share a state in a UInt8
-"""
-function check_neighbors(world_state::Matrix{Int32}, i::Int, j::Int)
-    m, n = size(world_state)
-
-    # check neighbors numbered clockwise
-    state = world_state[i, j]
-
-    neighbors = UInt8(0)
-    for (k, (i_off, j_off)) in enumerate(NEIGHBORS)
-        neighbors |= (state != getstate(world_state, i+i_off, j+j_off)) << (k-1)
-    end
-
-    # neighbors |= (state != getstate(world_state, i-1, j))
-    # neighbors |= (state != getstate(world_state, i-1, j+1)) << 1
-    # neighbors |= (state != getstate(world_state, i,   j+1)) << 2
-    # neighbors |= (state != getstate(world_state, i+1, j+1)) << 3
-    # neighbors |= (state != getstate(world_state, i+1, j))   << 4
-    # neighbors |= (state != getstate(world_state, i+1, j-1)) << 5
-    # neighbors |= (state != getstate(world_state, i,   j-1)) << 6
-    # neighbors |= (state != getstate(world_state, i-1, j-1)) << 7
-
-    return neighbors
-end
-
-
-function check_neighbors(world_state::Matrix{Int32})
-    m, n = size(world_state)
-    neighbors = zeros(UInt8, (m, n))
-
-    Threads.@threads for i in 1:m, j in 1:n
-        neighbors[i,j] = check_neighbors(world_state, i, j)
-    end
-
-    return neighbors
-end
-
-
-"""
 Data structure represented the current state of the world.
 """
 struct World
@@ -96,10 +49,9 @@ struct World
     # For each quadrant, a vector of tiles
     tiles::Vector{Vector{Tile}}
 
-    # For each pixel, record whether each of its 8 neighbors has a different
+    # For each pixel, record whether each of its 8 neighbors has the same
     # state, and encode in a UInt8
     neighbors::Matrix{UInt8}
-
 
     # cell type indexed by cell state + 1 (unoccpied is represented as its own
     # state)
@@ -117,7 +69,7 @@ end
 function World(m::Int, n::Int, ncelltype::Int, ncells::Int; nominal_tile_size::Int=80)
     # Generate a bunch of single pixel cells, placed uniformly at random.
     state = zeros(Int32, (m, n))
-    types = zeros(Int32, ncells+1)
+    types = zeros(Int32, ncells)
     volumes = zeros(Int32, ncells)
     areas = zeros(Int32, ncells)
 
@@ -125,10 +77,11 @@ function World(m::Int, n::Int, ncelltype::Int, ncells::Int; nominal_tile_size::I
     shuffle!(coords)
 
     for k in 1:ncells
-       state[(i, j)] = k
-       types[k] = rand(1:ncelltype)
-       volumes[k] = 1
-       areas[k] = 1
+        i, j = coords[k]
+        state[i, j] = k
+        types[k] = rand(1:ncelltype)
+        volumes[k] = 1
+        areas[k] = 1
     end
 
     # Build neighbors matrix
@@ -150,12 +103,12 @@ function World(m::Int, n::Int, ncelltype::Int, ncells::Int; nominal_tile_size::I
     for ytile in 1:nytiles, xtile in 1:nxtiles
         tile += 1
 
-        ya = (ytile-1)*nominal_tile_size
-        yb = min(ya + div(nominal_tile_size,2), m)
+        ya = (ytile-1)*nominal_tile_size+1
+        yb = min(ya + div(nominal_tile_size, 2) - 1, m)
         yc = min(ya + nominal_tile_size - 1, m)
 
-        xa = (xtile-1)*nominal_tile_size
-        xb = min(xa + div(nominal_tile_size,2), n)
+        xa = (xtile-1)*nominal_tile_size+1
+        xb = min(xa + div(nominal_tile_size, 2) - 1, n)
         xc = min(xa + nominal_tile_size - 1, n)
 
         tiles[1][tile] = (ya:yb, xa:xb)
@@ -171,22 +124,86 @@ function World(m::Int, n::Int, ncelltype::Int, ncells::Int; nominal_tile_size::I
 end
 
 
+Base.size(world::World) = size(world.state)
+
+
+"""
+Return the cell state at (i,j), and 0 if it's out of bounds.
+"""
+function getstate(world::World, i::Int, j::Int)
+    return getstate(world.state, i, j)
+end
+
+
+function getstate(world_state::Matrix{Int32}, i::Int, j::Int)
+    m, n = size(world_state)
+    return ((1 <= i <= m) && (1 <= j <= n)) ? world_state[i,j] : Int32(0)
+end
+
+
+function gettype(world::World, i::Int, j::Int)
+    state = getstate(world, i, j)
+    return state == 0 ? 0 : world.types[state]
+end
+
+
+"""
+Check each neighbor of (i,j) and record whether the share a state in a UInt8
+"""
+function check_neighbors(world_state::Matrix{Int32}, i::Int, j::Int)
+    m, n = size(world_state)
+
+    # check neighbors numbered clockwise
+    state = world_state[i, j]
+
+    neighbors = UInt8(0)
+    for (k, (i_off, j_off)) in enumerate(NEIGHBORS)
+        neighbors |= (state == getstate(world_state, i+i_off, j+j_off)) << (k-1)
+    end
+
+    # neighbors |= (state != getstate(world_state, i-1, j))
+    # neighbors |= (state != getstate(world_state, i-1, j+1)) << 1
+    # neighbors |= (state != getstate(world_state, i,   j+1)) << 2
+    # neighbors |= (state != getstate(world_state, i+1, j+1)) << 3
+    # neighbors |= (state != getstate(world_state, i+1, j))   << 4
+    # neighbors |= (state != getstate(world_state, i+1, j-1)) << 5
+    # neighbors |= (state != getstate(world_state, i,   j-1)) << 6
+    # neighbors |= (state != getstate(world_state, i-1, j-1)) << 7
+
+    return neighbors
+end
+
+
+function check_neighbors(world_state::Matrix{Int32})
+    m, n = size(world_state)
+    neighbors = zeros(UInt8, (m, n))
+
+    Threads.@threads for i in 1:m
+        for j in 1:n
+            neighbors[i,j] = check_neighbors(world_state, i, j)
+        end
+    end
+
+    return neighbors
+end
+
 
 """
 Evaluate the free energy function from scratch across the whole world
 """
 function loss(world::World, rules::RuleSet)
-    E_area = rules.elasticity_area * sum((rules.target_area[world.types] .- world.areas).^2)
-    E_vol = rules.elasticity_volume * sum((rules.target_volume[world.types] .- world.volumes).^2)
+    E_area = sum(rules.elasticity_area[world.types] .* (rules.target_area[world.types] .- world.areas).^2)
+    E_vol = sum(rules.elasticity_volume[world.types] .* (rules.target_volume[world.types] .- world.volumes).^2)
 
     E_adhesion = 0.0f0
     m, n = size(world.neighbors)
     for i in 1:m, j in 1:n
         neighbors = world.neighbors[i, j]
-        type = world.types[world.state[i, j]+1]
+        type = gettype(world, i, j)
+
         if neighbors != 0
             for (k, (i_off, j_off)) in enumerate(NEIGHBORS)
-                E_adhesion += rules.adhesion[type, world.types[getstate(world.state, i+i_off, j+j_off)+1]]
+                E_adhesion += rules.adhesion[type+1, gettype(world, i+i_off, j+j_off)+1]
             end
         end
     end
@@ -198,11 +215,11 @@ end
 
 
 function random_neighbor(neighbors::UInt8)
-    nth_nonzero = rand(1:count_ones(neighbors))
+    nth_zero = rand(1:count_zeros(neighbors))
     for i in 1:8
-        if (neighbors >> (i-1)) & 0x1 != 0
-            nth_nonzero -= 1
-            if nth_nonzero == 0
+        if (neighbors >> (i-1)) & 0x1 == 0
+            nth_zero -= 1
+            if nth_zero == 0
                 return i
             end
         end
@@ -215,9 +232,9 @@ end
 Compute the change in area of the cell occupying (i_source, j_source) from copying
 that state to (i_dest, j_dest.)
 """
-function Δarea(world::World, i_source::Int, j_source::Int, i_dest::Int, j_dest::Int)
+function Δsource_area(world::World, i_source::Int, j_source::Int, i_dest::Int, j_dest::Int)
     m, n = size(world.state)
-    state = world.state[i_source, j_source]
+    source_state = world.state[i_source, j_source]
 
     Δ = 0
 
@@ -226,7 +243,7 @@ function Δarea(world::World, i_source::Int, j_source::Int, i_dest::Int, j_dest:
     for (i_off, j_off) in NEIGHBORS
         i, j = i_dest + i_off, j_dest + j_off
         if 1 <= i <= m && 1 <= j <= n
-            if world.state[i, j] == state
+            if world.state[i, j] == source_state
                 neighbour_count += 1
                 if count_ones(world.neighbors[i, j]) == 1
                     Δ -= 1
@@ -244,37 +261,89 @@ function Δarea(world::World, i_source::Int, j_source::Int, i_dest::Int, j_dest:
 end
 
 
+function Δdest_area(world::World, i_source::Int, j_source::Int, i_dest::Int, j_dest::Int)
+    m, n = size(world.state)
+    dest_state = world.state[i_dest, j_dest]
+
+    Δ = 0
+
+    # Gain one area for any dest_state pixels that were surrounded but will be
+    # no longer.
+    neighbour_count = 0
+    for (i_off, j_off) in NEIGHBORS
+        i, j = i_dest + i_off, j_dest + j_off
+        if 1 <= i <= m && 1 <= j <= n
+            if world.state[i, j] == dest_state
+                neighbour_count += 1
+                if count_ones(world.neighbors[i, j]) == 8
+                    Δ += 1
+                end
+            end
+        end
+    end
+
+    # Lose one area if (i_dest, j_dest) was not surrounded
+    if neighbour_count != 8
+        Δ -= 1
+    end
+
+    return Δ
+end
+
+
 """
 Compute the change in energy from copying stat from (i_source, j_soruce) to
 (i_dest, j_dest)
 """
 function Δloss(
         world::World, rules::RuleSet, i_source::Int, j_source::Int, i_dest::Int, j_dest::Int)
-    state = world.state[i_source, j_source]
-    type = rules.types[state+1]
+
     m, n = size(world.state)
 
-    ΔE_vol = rules.elasticity_volume * (
-        (rules.target_volume[type] - (world.volumes[state] + 1))^2 -
-        (rules.target_volume[type] - world.volumes[state])^2)
+    source_state = getstate(world, i_source, j_source)
+    source_type = gettype(world, i_source, j_source)
 
-    # compute updated area
-    ΔE_area = rules.elasticity_area * (
-        (rules.target_area[type] - (world.areas[state] + Δarea(world, i_source, j_source, i_dest, j_dest)))^2 -
-        (rules.target_area[type] - world.areas[state])^2)
+    dest_state = getstate(world, i_dest, j_dest)
+    dest_type = gettype(world, i_dest, j_dest)
 
-    # recompute adhesion scores in light of (i_dest, j_dest) being set to type
+    ΔE_vol = 0f0
+    ΔE_area = 0f0
+
+    if source_type != 0
+        ΔE_vol += rules.elasticity_volume[source_type] * (
+            (rules.target_volume[source_type] - (world.volumes[source_state] + 1))^2 -
+            (rules.target_volume[source_type] - world.volumes[source_state])^2)
+
+        new_area = world.areas[source_state] + Δsource_area(world, i_source, j_source, i_dest, j_dest)
+        ΔE_area += rules.elasticity_area[source_type] * (
+            (rules.target_area[source_type] - new_area)^2 -
+            (rules.target_area[source_type] - world.areas[source_state])^2)
+
+    end
+
+    if dest_type != 0
+        ΔE_vol += rules.elasticity_volume[dest_type] * (
+            (rules.target_volume[dest_type] - (world.volumes[dest_state] - 1))^2 -
+            (rules.target_volume[dest_type] - world.volumes[dest_state])^2)
+
+        new_area = world.areas[dest_state] + Δdest_area(world, i_source, j_source, i_dest, j_dest)
+        ΔE_area += rules.elasticity_area[dest_type] * (
+            (rules.target_area[dest_type] - new_area)^2 -
+            (rules.target_area[dest_type] - world.areas[dest_state])^2)
+
+
+    end
+
+    # recompute adhesion scores in light of (i_dest, j_dest) being set to type `source_state`
     ΔE_adhesion = 0f0
-    old_type = world.types[world.state[i_dest, j_dest]+1]
     for (i_off, j_off) in NEIGHBORS
         i, j = i_dest + i_off, j_dest + j_off
-        if 1 <= i <= m && 1 <= j <= n
-            # Are we counting in both directions? Yes, afraid so.
-            neighbor_type = world.types[world.state[i,j]+1]
-            ΔE_adhesion -= rules.adhesion[neighbor_type, old_type]
-            ΔE_adhesion += rules.adhesion[neighbor_type, type]
-        end
+        neighbor_type = gettype(world, i, j)
+        ΔE_adhesion -= rules.adhesion[neighbor_type + 1, dest_type + 1]
+        ΔE_adhesion += rules.adhesion[neighbor_type + 1, source_type + 1]
     end
+
+    # @show (source_type, dest_type, ΔE_area, ΔE_vol, ΔE_adhesion)
 
     return ΔE_area + ΔE_vol + ΔE_adhesion
 end
@@ -284,13 +353,18 @@ end
 Advance the world by one iteration.
 """
 function tick(world::World, rules::RuleSet, E::Float32)
+    m, n = size(world)
+
     for quadrant in 1:4
-        Threads.@threads for (k, (yrange, xrange)) in enumerate(tiles[quadrant])
+        # Threads.@threads for (k, (yrange, xrange)) in enumerate(world.tiles[quadrant])
+        Threads.@threads for k in 1:length(world.tiles[quadrant])
+            yrange, xrange = world.tiles[quadrant][k]
+
             # Choose a random pixel with a border. (we do this just by rejection
             # sampling)
             i_source, j_source = rand(yrange), rand(xrange)
             attempts = 1000
-            while world.neighbors[i_source,j_source] && attempts > 0
+            while count_zeros(world.neighbors[i_source, j_source]) == 0 && attempts > 0
                 i_source, j_source = rand(yrange), rand(xrange)
                 attempts -= 1
             end
@@ -300,26 +374,47 @@ function tick(world::World, rules::RuleSet, E::Float32)
             end
 
             # select the random neighbor
-            i_dest, j_dest = random_neighbor(world.neighbors[i_source,j_source])
+            i_off, j_off = NEIGHBORS[random_neighbor(world.neighbors[i_source, j_source])]
+            i_dest, j_dest = i_source + i_off, j_source + j_off
 
             # Evaluate the energy of copying our state to the neighbors state
             ΔE = Δloss(world, rules, i_source, j_source, i_dest, j_dest)
 
             # accept?
             if ΔE < 0 || rand() < exp(-ΔE)
-                cell = world.state[i_source, j_source]
-                world.volumes[cell] += 1
-                world.areas[cell] += Δarea(world, i_source, j_source, i_dest, j_dest)
+                source_state = getstate(world, i_source, j_source)
+                dest_state = getstate(world, i_dest, j_dest)
 
-                world.state[i_dest, j_dest] = cell
+                if source_state != 0
+                    world.volumes[source_state] += 1
+                    world.areas[source_state] += Δsource_area(world, i_source, j_source, i_dest, j_dest)
+                end
 
-                ΔEs[k] = ΔE
+                if dest_state != 0
+                    world.volumes[dest_state] -= 1
+                    world.areas[dest_state] += Δdest_area(world, i_source, j_source, i_dest, j_dest)
+                end
+
+                world.state[i_dest, j_dest] = source_state
+
+                # Set neighbors for (i_dest, j_dest)
+                world.neighbors[i_dest, j_dest] = check_neighbors(world.state, i_dest, j_dest)
+
+                # Set neighbors for each of (i_dest, j_dests)'s neighbors
+                for (k, (i_off, j_off)) in enumerate(NEIGHBORS)
+                    i, j = i_dest + i_off, j_dest + j_off
+                    if 1 <= i <= m && 1 <= j <= n
+                        world.neighbors[i, j] = check_neighbors(world.state, i, j)
+                    end
+                end
+
+                world.ΔEs[k] = ΔE
             else
-                ΔEs[k] = 0f0
+                world.ΔEs[k] = 0f0
             end
         end
 
-        E += sum(ΔEs)
+        E += sum(world.ΔEs)
     end
 
     return E
