907543bdcbebccb5a1c17e77638584410de48011
Daniel C. Jones <dcjones@cs.washington.edu>
2021-08-18 13:18:43 -0700

    Write training examples to hdf5 file.

--- src/cgan.jl
+++ src/cgan.jl
@@ -16,15 +16,11 @@ function match_cells_imgs_filenames(
         imgs_filenames::Vector{String})
 
     pat = r"R\d+_X\d+_Y\d+"
-    @show first(cells_filenames)
-    @show match(pat, first(cells_filenames))
 
     cells_filenames_dict = Dict{String, String}(
         match(pat, filename).match => filename for filename in cells_filenames)
     imgs_filenames_dict = Dict{String, String}(
         match(pat, filename).match => filename for filename in imgs_filenames)
-
-    @show setdiff(keys(cells_filenames), keys(imgs_filenames))
 
     @assert isempty(setdiff(keys(cells_filenames), keys(imgs_filenames)))
 
@@ -105,27 +101,26 @@ function make_cgan_training_examples(
         cells_filenames::Vector{String},
         imgs_filenames::Vector{String},
         data_filename::String,
-        labels_filename::String,
+        labels_filename::String;
+        random_crops::Bool=true,
         crop_width::Int=256,
         crop_height::Int=256,
-        crop_coverage::Int=1)
+        crop_coverage::Int=1,
+        max_images::Int=typemax(Int),
+        h5_output_filename::Union{Nothing, String}="training-examples.h5",
+        png_output_path::Union{Nothing, String}=nothing)
 
     data = open(data_filename) do input
         JSON.parse(input)
     end
     focal_planes = data["focal_plane_selector"]
 
-    z_best = Dict{NamesTuple{(:r, :x, :y), Tuple{Int, Int, Int}}, Int}()
+    z_best = Dict{NamedTuple{(:r, :x, :y), Tuple{Int, Int, Int}}, Int}()
     for focal_plane in focal_planes
-        tile = (r=focal_plane["region_index"], x=focal_plane["tile_x"], y=focal_plane["tile_y"])
+        tile = (r=focal_plane["region_index"]+1, x=focal_plane["tile_x"]+1, y=focal_plane["tile_y"]+1)
         @assert !haskey(z_best, tile)
         z_best[tile] = focal_plane["best_z"]
     end
-
-    # TODO:
-    #  We want just those in the same region
-    #  Also need to match up "tile_x" and "tile_x"
-
 
     labels_df = CSV.read(labels_filename, DataFrame)
     labels = Dict{String, Int}()
@@ -144,9 +139,8 @@ function make_cgan_training_examples(
 
     count = 0
     for (cells_filename, imgs_filename) in cells_imgs_filenames
-        # TODO: small scale test
         count += 1
-        if count > 4
+        if count > max_images
             break
         end
 
@@ -154,26 +148,40 @@ function make_cgan_training_examples(
         imgs_mat = match(pat, imgs_filename)
         @assert cells_mat.captures == imgs_mat.captures
 
-        @show (cells_filename, imgs_filename)
-        println(cells_mat.match)
+        tile_r = parse(Int, imgs_mat.captures[1])
+        tile_x = parse(Int, imgs_mat.captures[2])
+        tile_y = parse(Int, imgs_mat.captures[3])
+        tile = (r=tile_r, x=tile_x, y=tile_y)
 
+        println("Reading ", imgs_filename)
         tiffimg = TiffImages.load(imgs_filename)
         channel_names, layer_channel, layer_focus = read_ome_tiff_description(tiffimg)
 
         img = Array{Float32}(tiffimg.data)
+
+        # throw out channels that are suboptimal focal lengths
+        channel_index = Int[]
+        for (i, (channel, focus)) in enumerate(zip(layer_channel, layer_focus))
+            if z_best[tile] == focus
+                push!(channel_index, i)
+            end
+        end
+
+        if isempty(channel_index)
+            error("""
+            No usable layers found. This is probably some kind of bug or a mismatch
+            between the tiff files and the 'data.json' file.
+            """)
+        end
+
+        img = img[:, :, channel_index]
         height, width, nchannels = size(img)
         mask = zeros(Float32, (height, width, ncelltypes))
-
-        # TODO: Now we want to throw out all the channels that correspond to
-        # suboptimal focal planes.
-
-        # TODO: Too many channels. We should read data.json to get the otpmizal
 
         celldata = open(cells_filename) do input
             JSON.parse(input)
         end
 
-        @show length(celldata)
         for (cellnum, cell) in celldata
             cell_id = @sprintf(
                 "R%s_X%s_Y%s_cell%s",
@@ -184,35 +192,67 @@ function make_cgan_training_examples(
             draw_polygon!(mask, poly, label)
         end
 
-        # Full tile images as training examples
-        push!(training_examples, (img, mask))
+        if random_crops
+            # TODO: it would be cool to also do e.g. random rotations, reflections, etc.
+            ncrops = round(Int, crop_coverage * (width * height) / (crop_width * crop_height), RoundUp)
+            for _ in 1:ncrops
+                x = rand(1:(width - crop_width + 1))
+                y = rand(1:(height - crop_height + 1))
 
-        # generate random crops of the full tile image
-        # TODO: it would be cool to also do e.g. random rotations, reflections, etc.
-        # ncrops = round(Int, crop_coverage * (width * height) / (crop_width * crop_height))
-        # for _ in 1:ncrops
-        #     x = rand(1:(width - crop_width + 1))
-        #     y = rand(1:(height - crop_height + 1))
+                mask_crop = mask[y:y+crop_height-1, x:x+crop_width-1, :]
+                img_crop = img[y:y+crop_height-1, x:x+crop_width-1, :]
 
-        #     mask_crop = mask[y:y+crop_height-1, x:x+crop_width-1, :]
-        #     img_crop = img[y:y+crop_height-1, x:x+crop_width-1, :]
+                # Don't bother training on examples with no cells
+                # (may have to revisit this, but we probably want to at least
+                # control how much empty training data we have)
+                if sum(img_crop) > 0
+                    push!(training_examples, (img_crop, mask_crop))
+                end
+            end
+        else
+            push!(training_examples, (img, mask))
+        end
 
-        #     # Don't bother training on examples with no cells
-        #     # (may have to revisit this, but we probably want to at least
-        #     # control how much empty training data we have)
-        #     if sum(img_crop) > 0
-        #         push!(training_examples, (img_crop, mask_crop))
-        #     end
-        # end
     end
 
-    dump_training_examples("training-examples", training_examples)
+    if png_output_path !== nothing
+        write_training_examples_png(png_output_path, training_examples)
+    end
+
+    if h5_output_filename !== nothing
+        write_training_examples_hdf5(h5_output_filename, training_examples)
+    end
 
     return training_examples
 end
 
 
-function dump_training_examples(
+"""
+Dump training examples to one big hdf5 file.
+"""
+function write_training_examples_hdf5(
+        filename::String, training_exampes::Vector{Tuple{Array{Float32, 3}, Array{Float32, 3}}})
+
+    n = length(training_exampes)
+    output = h5open(filename, "w")
+
+    img_group = create_group(output, "img")
+    mask_group = create_group(output, "mask")
+
+    for (i, (img, mask))  in enumerate(training_exampes)
+        key = @sprintf("%09d", i)
+        img_group[key] = img
+        mask_group[key] = mask
+    end
+
+    close(output)
+end
+
+
+"""
+Dump training data to a bunch of png images so we can inspect it.
+"""
+function write_training_examples_png(
         path::String, training_exampes::Vector{Tuple{Array{Float32, 3}, Array{Float32, 3}}})
     n = length(training_exampes)
     mkpath(path)
@@ -220,7 +260,6 @@ function dump_training_examples(
         # k = argmax(sum(img, dims=(1,2))[1,1,:])
         for k in 1:size(img, 3)
             open(joinpath(path, @sprintf("img-%09d-%03d.png", i, k)), "w") do output
-                @show maximum(img[:,:,k])
                 # save(Images.Stream{format"PNG"}(output), img[:,:,k] ./ maximum(img[:,:,k]))
                 save(Images.Stream{format"PNG"}(output), img[:,:,k])
             end
@@ -235,10 +274,12 @@ function dump_training_examples(
 end
 
 
+"""
+Scan line polygon filling algorithm. This is used for generating a mask image that
+one-hot encodes each pixel with the cell type (if any) it belongs to.
+"""
 function draw_polygon!(
         img::Array{Float32, 3}, poly::Vector{Tuple{Float32, Float32}}, fill::Int)
-
-    # TODO: Fuck, what if we just round to integers initially
 
     ymin, ymax = extrema([round(Int, p[2]) for p in poly])
     n = length(poly)
