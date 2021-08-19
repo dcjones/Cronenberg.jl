ead4a7e551665f1f8fb5c209a75d52867861c8d5
Daniel C. Jones <dcjones@cs.washington.edu>
2021-08-19 13:22:48 -0700

    Perhaps complete cgan implemenattion.

--- src/cgan.jl
+++ src/cgan.jl
@@ -1,10 +1,17 @@
-
 # Experimenting with fitting a cgan to try to generate realistic looking images.
 
 import CSV
 import JSON
-import TiffImages
 import LightXML
+import TiffImages
+import Zygote
+using Flux
+using Flux.Losses: logitbinarycrossentropy, mae
+using Flux.Optimise: ADAM
+using StaticArrays
+
+const device = gpu
+# const device = cpu
 
 
 """
@@ -106,6 +113,7 @@ function make_cgan_training_examples(
         crop_width::Int=256,
         crop_height::Int=256,
         crop_coverage::Int=1,
+        min_crop_masked_prop::Float64=0.05,
         max_images::Int=typemax(Int),
         h5_output_filename::Union{Nothing, String}="training-examples.h5",
         png_output_path::Union{Nothing, String}=nothing)
@@ -202,17 +210,20 @@ function make_cgan_training_examples(
                 mask_crop = mask[y:y+crop_height-1, x:x+crop_width-1, :]
                 img_crop = img[y:y+crop_height-1, x:x+crop_width-1, :]
 
-                # Don't bother training on examples with no cells
+                # Don't bother training on examples with few or any cells
                 # (may have to revisit this, but we probably want to at least
                 # control how much empty training data we have)
-                if sum(img_crop) > 0
+                if sum(mask_crop)/(crop_height*crop_width) > min_crop_masked_prop
                     push!(training_examples, (img_crop, mask_crop))
                 end
             end
         else
             push!(training_examples, (img, mask))
         end
+    end
 
+    if isempty(training_examples)
+        error("No training examples. Possibly the input images are too sparsely populated.")
     end
 
     if png_output_path !== nothing
@@ -236,14 +247,23 @@ function write_training_examples_hdf5(
     n = length(training_exampes)
     output = h5open(filename, "w")
 
-    img_group = create_group(output, "img")
-    mask_group = create_group(output, "mask")
+    imgs = cat([img for (img, mask) in training_exampes]..., dims=4)
+    masks = cat([mask for (img, mask) in training_exampes]..., dims=4)
 
-    for (i, (img, mask))  in enumerate(training_exampes)
-        key = @sprintf("%09d", i)
-        img_group[key] = img
-        mask_group[key] = mask
-    end
+    output["imgs"] = imgs
+    output["masks"] = masks
+
+    # Or we could write each to a seperate dataset, which might save memory
+    # but will be less convenient.
+
+    # img_group = create_group(output, "img")
+    # mask_group = create_group(output, "mask")
+
+    # for (i, (img, mask))  in enumerate(training_exampes)
+    #     key = @sprintf("%09d", i)
+    #     img_group[key] = img
+    #     mask_group[key] = mask
+    # end
 
     close(output)
 end
@@ -331,3 +351,285 @@ function draw_polygon!(
         end
     end
 end
+
+
+function read_training_data(training_data_filename::String, batchsize::Int=100)
+    input = h5open(training_data_filename)
+
+    masks = read(input["masks"])
+    imgs = read(input["imgs"])
+
+    maskdepth = size(masks, 3)
+    imgdepth = size(imgs, 3)
+
+    trainingdata = Flux.Data.DataLoader((masks, imgs), batchsize=batchsize)
+
+    close(input)
+
+    return trainingdata, maskdepth, imgdepth
+end
+
+
+# U-Net downsample block
+# Applies a `filters`-by-`filters` convolution changing input depth `indepth` to
+# output depth `outdepth`
+function downsample(filters::Int, indepth::Int, outdepth::Int; batchnorm=true)
+    layers = Any[]
+    push!(layers, Conv(
+        (filters, filters),
+        indepth => outdepth,
+        stride=2,
+        # pad=SamePad(),
+        pad=1,
+        init=(dims...) -> 0.02f0 .* randn(Float32, dims...),
+        bias=false))
+
+    if batchnorm
+        push!(layers, BatchNorm(outdepth))
+    end
+
+    push!(layers, x -> leakyrelu.(x))
+
+    return Chain(layers...)
+end
+
+
+# U-Net upsample block
+# Does transpose convolutions to double the size of the input.
+function upsample(filters::Int, indepth::Int, outdepth::Int; dropout=false)
+    layers = Any[]
+    push!(layers, ConvTranspose(
+        (filters, filters),
+        indepth => outdepth,
+        stride=2,
+        pad=SamePad(),
+        init=(dims...) -> 0.02f0 .* randn(Float32, dims...),
+        bias=false))
+
+    push!(layers, BatchNorm(outdepth))
+
+    if dropout
+        push!(layers, Dropout(0.5))
+    end
+
+    push!(layers, x -> relu.(x))
+
+    return Chain(layers...)
+end
+
+
+struct Discriminator
+    downsample_block
+    patch_block
+    output_layer
+end
+
+
+function Discriminator(maskdepth::Int, imgdepth::Int)
+    # mask and image are concatenated to get an input of [256, 256, maskdepth+imgdepth, B]
+    downsample_block = Chain(
+        downsample(4, maskdepth+imgdepth, 64, batchnorm=false), # [128, 128, 64, B]
+        downsample(4, 64, 128), # [64, 64, 128, B]
+        downsample(4, 128, 256)) |> device # [32, 32, 256, B]
+
+    # pad with zeros to get [34, 34, 256, B]
+
+    patch_block = Chain(
+        Conv(
+            (4,4),
+            256 => 512,
+            stride=1,
+            init=(dims...) -> 0.02f0 .* randn(Float32, dims...),
+            bias=false), # [31, 31, 512, B]
+        BatchNorm(512), # [31, 31, 512, B]
+        x -> leakyrelu.(x)) |> device
+
+    # pad with zeros to get [33, 33, 512, B]
+
+    output_layer = Conv(
+        (4, 4), 512 => 1,  stride=1,
+        init=(dims...) -> 0.02f0 .* randn(Float32, dims...)) |> device # [30, 30, 1, B]
+
+    return Discriminator(downsample_block, patch_block, output_layer)
+end
+
+
+function (disc::Discriminator)(mask::AbstractArray, img::AbstractArray)
+    input = cat(mask, img, dims=3)
+    input = disc.downsample_block(input) # [32, 32, 512, B]
+    input = pad_zeros(input, (1, 1, 1, 1, 0, 0, 0, 0)) # [34, 34, 512, B]
+    input = disc.patch_block(input) # [31, 31, 512, B]
+    input = pad_zeros(input, (1, 1, 1, 1, 0, 0, 0, 0)) # [33, 33, 512, B]
+    return disc.output_layer(input) # [30, 30, 1, B]
+end
+
+
+function discriminator_loss(real_disc, fake_disc)
+    real_loss = Flux.Losses.logitbinarycrossentropy(real_disc, 1f0)
+    fake_loss = Flux.Losses.logitbinarycrossentropy(fake_disc, 0f0)
+    return real_loss + fake_loss
+end
+
+struct Generator
+    downsample_layers::SVector{8, Chain}
+    upsample_layers::SVector{7, Chain}
+    output_layer::ConvTranspose
+end
+
+
+function Generator(maskdepth::Int, imgdepth::Int)
+    # Input assumed to be [256, 256, indepth, B]
+    downsample_layers = SA[
+        downsample(4, maskdepth, 64, batchnorm=false), # [128, 128, 64, B]
+        downsample(4, 64, 128),  # [64, 64, 128, B]
+        downsample(4, 128, 256), # [32, 32, 256, B]
+        downsample(4, 256, 512), # [16, 16, 512, B]
+        downsample(4, 512, 512), # [8, 8, 512, B]
+        downsample(4, 512, 512), # [4, 4, 512, B]
+        downsample(4, 512, 512), # [2, 2, 512, B]
+        downsample(4, 512, 512) # [1, 1, 512, B]
+    ] .|> device
+
+    # Note, input depths are doubled due to skip connections
+    upsample_layers = SA[
+        upsample(4, 512, 512, dropout=true), # [2, 2, 512, B]
+        upsample(4, 1024, 512, dropout=true), # [4, 4, 512, B]
+        upsample(4, 1024, 512, dropout=true), # [8, 8, 512, B]
+        upsample(4, 1024, 512), # [16, 16, 512, B]
+        upsample(4, 1024, 256), # [32, 32, 256, B]
+        upsample(4, 512, 128), # [64, 64, 128, B]
+        upsample(4, 256, 64), # [128, 128, 64, B]
+    ] .|> device
+
+    output_lyr = ConvTranspose(
+        (4, 4),
+        128 => imgdepth,
+        tanh,
+        stride=2,
+        pad=SamePad(),
+        init=(dims...) -> 0.02f0 .* randn(Float32, dims...)) |> device
+
+    return Generator(
+        downsample_layers, upsample_layers, output_lyr)
+end
+
+
+function (gen::Generator)(masks::AbstractArray)
+    # Downsample
+    input = masks
+    skips = Any[]
+    for lyr in gen.downsample_layers
+        input = lyr(input)
+        push!(skips, input)
+    end
+
+    pop!(skips)
+    reverse!(skips)
+
+    @show size(input)
+
+    # Upsample
+    for (lyr, skip) in zip(gen.upsample_layers, skips)
+        input = lyr(input)
+        @show (typeof(input), typeof(skip))
+        @show (size(input), size(skip))
+        input = cat(input, skip, dims=3)
+    end
+
+    return gen.output_layer(input)
+end
+
+
+function generator_loss(
+        real_imgs::AbstractArray, fake_imgs::AbstractArray,
+        fake_disc::AbstractArray, λ::Float32)
+    gan_loss = binarycrossentropy(fake_disc, 1f0)
+    l1_loss = mae(real_imgs, fake_imgs)
+
+    return gan_loss + (λ * l1_loss)
+end
+
+
+function train_discriminator(
+        disc_opt, disc::Discriminator,
+        masks::AbstractArray, real_imgs::AbstractArray, fake_imgs::AbstractArray)
+    ps = params(disc.downsample_block, disc.patch_block, disc.output_layer)
+    loss, back = Zygote.pullback(ps) do
+        real_disc = disc(masks, real_imgs)
+        fake_disc = disc(masks, fake_imgs)
+        return discriminator_loss(real_disc, fake_disc)
+    end
+    grads = back(1f0)
+    update!(disc_opt, ps, grads)
+    return loss
+end
+
+Zygote.@nograd train_discriminator
+
+
+"""
+Take one gradient descent step on one batch of training data.
+"""
+function train_step(
+        gen_opt, disc_opt,
+        gen::Generator, disc::Discriminator,
+        masks::AbstractArray, real_imgs::AbstractArray;
+        λ::Float32=100f0)
+
+    ps = params(gen.downsample_layers, gen.upsample_layers, gen.output_layer)
+    loss["gen"], back = Zygote.pullback(ps) do
+        fake_imgs = gen(masks)
+        loss["disc"] = train_discriminator(disc_opt, disc, masks, real_imgs, fake_imgs)
+        generator_loss(real_imgs, fake_imgs, disc(masks, fake_imgs), λ)
+    end
+    grads = back(1f0)
+    update!(gen_opt, ps, grads)
+    return loss
+end
+
+
+"""
+Train a pix2pix style cGAN on paired training data saved to an HDF5 file. Write
+parameters to another hdf5 file.
+"""
+function train_cgan(
+        training_data_filename::String;
+        nepochs::Int=100,
+        batchsize::Int=100,
+        λ::Float32=100f0)
+
+    trainingdata, maskdepth, imgdepth = read_training_data(
+        training_data_filename, batchsize)
+
+    gen = Generator(maskdepth, imgdepth)
+    disc = Discriminator(maskdepth, imgdepth)
+
+    # Make sure I can run these
+    (masks, real_imgs) = first(trainingdata)
+    @show (maskdepth, imgdepth)
+    @show size(masks)
+    @show size(gen(masks |> device))
+    exit()
+
+    gen_opt = ADAM()
+    disc_opt = ADAM()
+
+    for epoch in 1:nepochs
+        println("Epoch: ", epoch)
+        for (masks, real_imgs) in trainingdata
+            #  TODO: Do I need to send real_imgs, masks to gpu?
+
+            loss = train_step(
+                gen_opt, disc_opt, gen, disc,
+                masks |> device, real_imgs |> device,
+                λ=λ)
+
+            # TODO: report stuff occasionally
+            @show loss
+        end
+    end
+
+    # TODO: remember we have to use `testmode!` to disable dropout once
+    # we actually use the model.
+end
+
